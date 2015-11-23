6cf3c361f355061f7e016664b9b996aca6ca96a0
Ramin Halviatti <ramin@outlook.com>
2015-11-22 19:27:08 -0800

    Reworked finding seam using shortest path algorithm, and completed removal of seams.

--- 7_seam_carver/SeamCarver.java
+++ 7_seam_carver/SeamCarver.java
@@ -28,32 +28,30 @@ import java.awt.Color;
 import edu.princeton.cs.algs4.Picture;
 import edu.princeton.cs.algs4.StdOut;
 
-public class SeamCarver {
-    private int[][] pixelArray; // Internal representation of a picture 
-                                // where Alpha, Red, Green and Blue color
-                                // values are packed in an integer for each pixel
-    private int H;              // Height of picture
-    private int W;              // Width of picture
-    private double[][] energyArray; // Calculated energy values for each pixel in pixelArray
+public class SeamCarver {   
+    private int[][] pixels; // Alpha and RGB values packed in integers
+    private int H;          // Height of picture
+    private int W;          // Width of picture
+    private static final double MAX_ENERGY  = 1000.00;
+    private static final boolean HORIZONTAL = true;
+    private static final boolean VERTICAL   = false;
 
     /**
      * create a seam carver object based on the given picture.
      * @param picture the picture
      */
     public SeamCarver(Picture picture) {
-        // Set width and height
+        if (picture == null)
+            throw new NullPointerException(
+                "Invalid null argument was passed.");
+
         W = picture.width();
         H = picture.height();
         // Create a pixel array based on the picture ARGB values
-        pixelArray = new int[W][H];
+        pixels = new int[W][H];
         for (int col = 0; col < W; col++)
             for (int row = 0; row < H; row++) 
-                pixelArray[col][row] = picture.get(col, row).getRGB();
-        // Calculate energy values for each pixel
-        energyArray = new double[W][H];
-        for (int col = 0; col < W; col++)
-            for (int row = 0; row < H; row++) 
-                energyArray[col][row] = calculateEnergy(col, row);
+                pixels[col][row] = picture.get(col, row).getRGB();
     }
 
      /**
@@ -63,11 +61,12 @@ public class SeamCarver {
         Picture picture = new Picture(W, H);
         for (int col = 0; col < W; col++)
             for (int row = 0; row < H; row++) {
-                Color color = new Color(pixelArray[col][row], true);
+                Color color = new Color(pixels[col][row], true);
                 picture.set(col, row, color);
             }
         return picture;
     }
+
      /**
      * Return width of internal picture.
      */
@@ -88,107 +87,177 @@ public class SeamCarver {
      * @param y is the row
      */
     public  double energy(int x, int y)  {
+        // Validate params
         if (x < 0 || x >= width() || y < 0 || y >= height())
             throw new IndexOutOfBoundsException(
                 "x or y is outside its prescribed range.");
 
-        return energyArray[x][y];
-    }
+        // Border pixels get energy of MAX_ENERGY by default
+        if (x == 0 || y == 0 || x == (width()-1) || y == (height()-1)) 
+            return MAX_ENERGY;
 
-     /**
-     * Calculate energy for a passed in (x,y)
-     * @param int x is the column
-     * @param int y is the row
-     */
-    private double calculateEnergy(int x, int y) {
-        if (x == 0 || y == 0 || x == (width()-1) || y == (height()-1)) {
-            // Border pixels get energy of 1000 by default
-            return 1000.0;
-        } 
         // Energy is square root of gradiant squared for x and y
         return Math.sqrt(
-            gradiantSquare(x, y, true) + gradiantSquare(x, y, false));        
+            gradiantSquare(x, y, HORIZONTAL) + gradiantSquare(x, y, VERTICAL));        
     }
 
      /**
+     * Return the sequence of indices for horizontal seam
+     */
+    public int[] findHorizontalSeam() {
+        double[][] energyTable = createEnergyTable(H, W, true);
+        return findSeam(energyTable);
+    }
+
+     /**
+     * Return the sequence of indices for vertical seam
+     */
+    public int[] findVerticalSeam() {
+        double[][] energyTable = createEnergyTable(W, H, false);
+        return findSeam(energyTable);
+    }
+
+     /**
+     * Helper function that does finds a seam depending on the passed in energyArray
+     */
+    private int[] findSeam(double[][] energyArray) {
+        int W = energyArray.length;
+        int H = energyArray[0].length;
+        double[][] distTo = new double[W][H];
+        for (int x = 0; x < W; x++) {            
+            for (int y = 0; y < H; y++) 
+                if (y == 0) distTo[x][y] = MAX_ENERGY;
+                else distTo[x][y] = Double.POSITIVE_INFINITY;
+            }        
+        
+        int[] seam = new int[H];
+        int[][] edgeTo = new int[W][H];
+        
+        // Populate distTo while relaxing edges
+        for (int y = 0; y < H - 1; y++) 
+            for (int x = 0; x < W; x++) 
+                for (int k = x-1; k <= x+1; k++) 
+                    if (k >= 0 && k < W) 
+                        if (distTo[k][y+1] > distTo[x][y] + energyArray[k][y+1]) {
+                            // relax edge
+                            distTo[k][y+1] = distTo[x][y] + energyArray[k][y+1];
+                            edgeTo[k][y+1] = y*energyArray.length + x;
+                        }
+
+        // find minimum in last row
+        double lastMin = Double.POSITIVE_INFINITY;
+        int lastIndex = -1;
+        for (int x = 0; x < W; x++) {
+            if (lastMin > distTo[x][H-1]) {
+                lastMin = distTo[x][H-1];
+                lastIndex = x;
+            }        
+        }
+        seam[H-1] = lastIndex;
+        
+        // reconstruct the path from bottom up
+        for (int y = H-1; y > 0; y--)
+            seam[y-1] = edgeTo[seam[y]][y] % W;        
+        return seam;
+    }
+
+     /**
+     * Remove horizontal seam from current picture
+     * @param seam is the array of indices to be removed
+     */
+    public    void removeHorizontalSeam(int[] seam) {
+        if (seam == null)
+            throw new NullPointerException(
+                "Invalid null argument was passed.");
+        
+        if (seam.length != W || H <= 1) 
+            throw new IllegalArgumentException(
+                "Invalid seam or picture size.");        
+
+        int[][] pixelsCopy = new int[W][H-1];
+        
+        for (int col = 0; col < W; col++) {
+            System.arraycopy(pixels[col], 0, 
+                             pixelsCopy[col], 0, 
+                             seam[col]);
+            System.arraycopy(pixels[col], seam[col]+1, 
+                             pixelsCopy[col], seam[col], 
+                             H-seam[col]-1);
+        }
+        --H;
+        pixels = pixelsCopy;
+    }  
+
+     /**
+     * Remove vertical seam from current picture
+     * @param seam is the array of indices to be removed
+     */
+    public    void removeVerticalSeam(int[] seam) {
+        if (seam == null)
+            throw new NullPointerException(
+                "Invalid null argument was passed.");
+
+        if (seam.length != H || W <= 1) 
+            throw new IllegalArgumentException(
+                "Invalid seam or picture size.");  
+
+        int[][] pixelsCopy = new int[W-1][H];
+
+        for (int x = 0; x < W; x++)
+            for (int y = 0; y < H; y++) {
+                if (x < seam[y]) 
+                    pixelsCopy[x][y] = pixels[x][y];
+                else if (x > seam[y]) 
+                    pixelsCopy[x-1][y] = pixels[x][y];
+            }
+
+        --W;
+        pixels = pixelsCopy;      
+
+    } 
+
+    /**
      * Helper function that calculates gradient square values for a passed in (x,y)
      * @param int x is the column
      * @param int y is the row
      * @param boolean forX denotes if the calcution is for x (or y if false)
      */
-    private int gradiantSquare(int x, int y, boolean forX) {
+    private int gradiantSquare(int x, int y, boolean horizontal) {
         int pixelBefore;
         int pixelAfter;
-        if (forX) {
+        if (horizontal) {
             assert( x > 0 && x < width() );
-            pixelBefore = pixelArray[x - 1][y];
-            pixelAfter  = pixelArray[x + 1][y];
+            pixelBefore = pixels[x - 1][y];
+            pixelAfter  = pixels[x + 1][y];
         }
         else {
             assert( y > 0 && y < height() );
-            pixelBefore = pixelArray[x][y - 1];
-            pixelAfter  = pixelArray[x][y + 1];
+            pixelBefore = pixels[x][y - 1];
+            pixelAfter  = pixels[x][y + 1];
         }
-        int redDiff = ((pixelAfter  & 0x00FF0000) >> 16) - 
-                      ((pixelBefore & 0x00FF0000) >> 16);
-        int greenDiff = ((pixelAfter  & 0x0000FF00) >> 8) -
-                        ((pixelBefore & 0x0000FF00) >> 8);
-        int blueDiff = (pixelAfter & 0x000000FF) - 
-                       (pixelBefore & 0x000000FF);
+        int redDiff = Math.abs(((pixelAfter  & 0x00FF0000) >> 16) - 
+                               ((pixelBefore & 0x00FF0000) >> 16));
+        int greenDiff = Math.abs(((pixelAfter  & 0x0000FF00) >> 8) -
+                                 ((pixelBefore & 0x0000FF00) >> 8));
+        int blueDiff = Math.abs((pixelAfter & 0x000000FF) - 
+                                (pixelBefore & 0x000000FF));
         return (redDiff*redDiff + greenDiff*greenDiff + blueDiff*blueDiff);
     }
 
-/*
-    // sequence of indices for horizontal seam
-    public   int[] findHorizontalSeam() {
-
+    private double[][] createEnergyTable(int width, int height, boolean isTranspose)
+    {
+        double[][] table = new double[width][height];
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++) {
+                if (isTranspose) 
+                    table[x][y] = energy(y, x);
+                else 
+                    table[x][y] = energy(x, y);
+            }
+        return table;        
     }
-*/
-    // sequence of indices for vertical seam
-    public   int[] findVerticalSeam() {
-        int[] seam = new int[height()];
-        // Find minimum
-        double min = 1001.0;
-        int minCol = -1;
-        for (int col = 1; col < width() - 1; col++) {
-            if (energyArray[col][1] < min) {
-                min = energyArray[col][1];
-                minCol = col;
-            }
-        }
-        StdOut.printf("Min is 1000.0, index: (%d, 0)\n", minCol);
-        StdOut.printf("Min is %f, index: (%d, 1)\n", min, minCol);
-        seam[0] = seam[1] = minCol;
 
-        for (int row = 2; row < height() - 1; row++) { 
-            min = 1001.0;
-            int minIndex = -1;
-            for (int i = 1; i >= -1; i--) {
-                if (energyArray[minCol + i][row] < min) {
-                    minIndex = minCol + i;
-                    min = energyArray[minIndex][row];
-                }
-            }
-            minCol = minIndex;
-            StdOut.printf("Min is %f, index: (%d, %d)\n", min, minCol, row);
-            seam[row] = minCol;
-        }
-        StdOut.printf("Min is 1000.0, index: (%d, %d)\n", minCol, height()-1);
-        seam[height()-1] = minCol;
-        return seam;
-    }   
-
-    // remove horizontal seam from current picture
-    public    void removeHorizontalSeam(int[] seam) {
-
-    }  
-
-    // remove vertical seam from current picture
-    public    void removeVerticalSeam(int[] seam) {
-
-    } 
-
-    private static void validateEnergy(SeamCarver sc) {
+   private static void validateEnergy(SeamCarver sc) {
         // Expected energy results for test_data/6x5.png
         double[][] energyData = {
             {1000.00, 1000.00, 1000.00, 1000.00, 1000.00, 1000.00}, 
